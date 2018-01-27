627d2561d1d9608ef358467b3adf2508e675ead7
James Atkin <jamesalexatkin@gmail.com>
2018-01-27 21:56:36 +0000

    Added some physics methods

--- src/org/alien8/core/Entity.java
+++ src/org/alien8/core/Entity.java
@@ -3,53 +3,81 @@ package org.alien8.core;
 import org.alien8.core.geometry.Position;
 
 public abstract class Entity {
-	private Position position;
-	private ID id;
+  private Position position;
+  private ID id;
+  private double mass;
+  private double speed;
+  private double direction;
 
-	/**
-	 * Basic constructor for an entity
-	 * 
-	 * @param position the XY coordinates for this entity
-	 * @param id the ID of this entity
-	 */
-	public Entity(Position position, ID id) {
-		this.setPosition(position);
-		this.setId(id);
-	}
-	/**
-	 * Basic constructor for an entity
-	 * 
-	 * @param x X coordinate for this entity
-	 * @param y Y coordinate for this entity
-	 * @param id the ID of this entity
-	 */
-	public Entity(double x, double y, ID id) {
-		this.setPosition(new Position(x,y));
-		this.setId(id);
-	}
+  /**
+   * Basic constructor for an entity
+   * 
+   * @param position the XY coordinates for this entity
+   * @param id the ID of this entity
+   */
+  public Entity(Position position, ID id) {
+    this.setPosition(position);
+    this.setId(id);
+  }
 
-	/**
-	 * @return the position in XY coordinates
-	 */
-	public Position getPosition() {
-		return position;
-	}
+  /**
+   * Basic constructor for an entity
+   * 
+   * @param x X coordinate for this entity
+   * @param y Y coordinate for this entity
+   * @param id the ID of this entity
+   */
+  public Entity(double x, double y, ID id) {
+    this.setPosition(new Position(x, y));
+    this.setId(id);
+  }
 
-	/**
-	 * @param position the position to set, in XY coordinates
-	 */
-	public void setPosition(Position position) {
-		this.position = position;
-	}
+  /**
+   * @return The position in XY coordinates
+   */
+  public Position getPosition() {
+    return position;
+  }
 
-	/**
-	 * @return the id of this entity
-	 */
-	public ID getId() {
-		return id;
-	}
-	
-	private void setId(ID id) {
-		this.id = id;
-	}
+  /**
+   * @param Position the position to set, in XY coordinates
+   */
+  public void setPosition(Position position) {
+    this.position = position;
+  }
+
+  /**
+   * @return The id of this entity
+   */
+  public ID getId() {
+    return id;
+  }
+
+  private void setId(ID id) {
+    this.id = id;
+  }
+
+  public double getMass() {
+    return mass;
+  }
+
+  public void setMass(double mass) {
+    this.mass = mass;
+  }
+
+  public double getSpeed() {
+    return speed;
+  }
+
+  public void setSpeed(double speed) {
+    this.speed = speed;
+  }
+
+  public double getDirection() {
+    return direction;
+  }
+
+  public void setDirection(double direction) {
+    this.direction = direction;
+  }
 }
