564e8d38f20393d394a593afc3d2bfff09e0a92d
ajb614 <ajb614@cs.bham.ac.uk>
2018-01-30 02:54:53 +0000

    Added client initialisation code and game window

---
+++ src/org/alien8/rendering/Renderer.java
@@ -0,0 +1,33 @@
+package org.alien8.rendering;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Renderer extends Canvas{
+
+	private static final long serialVersionUID = 1L;
+	
+	private int width;
+	private int height;
+	
+	private JFrame frame;
+	
+	public Renderer(Dimension size){
+		
+		setPreferredSize(size);
+		width = size.width;
+		height = size.height;
+		
+		frame = new JFrame();
+		frame.setTitle("Battleship Antarctica");
+		frame.setResizable(false);
+		frame.add(this);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+}
