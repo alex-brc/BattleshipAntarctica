6087fdfa3d8fcf1cc3dc94cfcf081df27e180202
ajb614 <ajb614@cs.bham.ac.uk>
2018-02-05 23:51:11 +0000

    Added basic rendering code

--- src/org/alien8/rendering/Renderer.java
+++ src/org/alien8/rendering/Renderer.java
@@ -1,7 +1,13 @@
 package org.alien8.rendering;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
 import javax.swing.JFrame;
 import org.alien8.managers.InputManager;
 
@@ -13,12 +19,18 @@ public class Renderer extends Canvas {
   private int height;
 
   private JFrame frame;
+  
+  private BufferedImage image; //image which is rendered onto canvas
+  private int[] pixels;
 
   public Renderer(Dimension size) {
 
     setPreferredSize(size);
     width = size.width;
     height = size.height;
+    
+    image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 
     addMouseListener(InputManager.getInstance());
     addMouseMotionListener(InputManager.getInstance());
@@ -38,6 +50,28 @@ public class Renderer extends Canvas {
    * The render() method renders all entities to the screen in their current state
    */
   public void render() {
-
+    BufferStrategy bs = getBufferStrategy(); //gets canvas buffer strategy
+	if (bs == null){
+	  createBufferStrategy(3); //if none found, create a triple buffering strategy
+	  requestFocus();
+	  return;
+	}
+	
+	clear();
+	
+	Graphics g = bs.getDrawGraphics(); //graphics object from buffer strategy
+	
+	g.setColor(Color.BLACK);
+	g.fillRect(0, 0, getWidth(), getHeight()); //background rectangle same size as canvas
+	g.drawImage(image, 0 , 0 , getWidth(), getHeight(), null); //draw image with pixel data from image raster
+	//g.fillRect(Mouse.getX(), Mouse.getY(), 64, 64);
+	g.dispose(); //necessary to clear memory
+	bs.show(); //displays the buffer strategy to the monitor
+  }
+  
+  public void clear(){
+	for (int i = 0; i < pixels.length; i++){
+	  pixels[i] = 0; //cycles through all pixels and sets them to 0, resetting the array
+	}
   }
 }
