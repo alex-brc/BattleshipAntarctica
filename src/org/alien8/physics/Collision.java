50d22a65c122773f92964b8026cb1520b255566e
James Atkin <jamesalexatkin@gmail.com>
2018-02-10 16:57:34 +0000

    Stopped ship going out of bounds

--- src/org/alien8/physics/Collision.java
+++ src/org/alien8/physics/Collision.java
@@ -8,10 +8,17 @@ import org.alien8.ship.Ship;
 public class Collision {
   private Entity entity1;
   private Entity entity2;
+  private MTV mtv;
 
   public Collision(Entity entity1, Entity entity2) {
     this.entity1 = entity1;
     this.entity2 = entity2;
+  }
+
+  public Collision(Entity entity1, Entity entity2, MTV mtv) {
+    this.entity1 = entity1;
+    this.entity2 = entity2;
+    this.mtv = mtv;
   }
 
   public Entity getEntity1() {
@@ -29,19 +36,67 @@ public class Collision {
   public void resolveCollision() {
     // Ship-Ship collision
     if ((entity1 instanceof Ship) & (entity2 instanceof Ship)) {
-      System.out.println("Two ships have collided!");
+      // System.out.println("Two ships have collided!");
       double speed1 = entity1.getSpeed();
       double speed2 = entity2.getSpeed();
       double direction1 = entity1.getDirection();
       double direction2 = entity2.getDirection();
 
+      // Move ships apart according to the Minimum Translation Vector
+      // System.out.println("Cur" + entity1.getPosition());
+      // mod 10 allows distance to be scaled down so that objects don't fly away from each other as
+      // much
+      double mtvX = mtv.getDistance() * mtv.getAxis().getX() % 10;
+      double mtvY = mtv.getDistance() * mtv.getAxis().getY() % 10;
+
+      Position pos1 = entity1.getPosition();
+      Position pos2 = entity2.getPosition();
+
+      double xdiff1 = 0, xdiff2 = 0, ydiff1 = 0, ydiff2 = 0;
+
+      if (pos1.getX() > pos2.getX()) {
+        xdiff1 = mtvX;
+        xdiff2 = -mtvX;
+      } else {
+        xdiff1 = -mtvX;
+        xdiff2 = mtvX;
+      }
+
+      if (pos1.getY() > pos2.getY()) {
+        ydiff1 = mtvY;
+        ydiff2 = -mtvY;
+
+      } else {
+        ydiff1 = -mtvX;
+        ydiff2 = mtvX;
+      }
+
+      entity1.setPosition(new Position(pos1.getX() + xdiff1, pos1.getY() + ydiff1));
+      entity1.translateObb(xdiff1, ydiff1);
+      // System.out.println("New " + entity1.getPosition());
+      entity2.setPosition(new Position(pos2.getX() + xdiff2, pos2.getY() + ydiff2));
+      entity2.translateObb(xdiff2, ydiff2);
+
+
+      // Position newPos1 =
+      // new Position(entity1.getPosition().getX() + mtvX, entity1.getPosition().getY() + mtvY);
+      // entity1.setPosition(newPos1);
+      // entity1.translateObb(mtvX, mtvY);
+      // System.out.println("New " + entity1.getPosition());
+      // Position newPos2 =
+      // new Position(entity2.getPosition().getX() - mtvX, entity2.getPosition().getY() - mtvY);
+      // entity2.setPosition(newPos2);
+      // entity2.translateObb(-mtvX, -mtvY);
+
       // Assume for the moment that ships have the same mass
       // This means that we can just swap their speed and direction
       // We multiply speed by the coefficient of restitution to decrease it
       entity1.setSpeed(speed2 * Parameters.RESTITUTION_COEFFICIENT);
-      entity1.setDirection(direction2);
+      PhysicsManager.rotateEntity(entity1, (direction1 - direction2) % 5);
+      // entity1.setDirection(direction2);
       entity2.setSpeed(speed1 * Parameters.RESTITUTION_COEFFICIENT);
-      entity2.setDirection(direction1);
+      PhysicsManager.rotateEntity(entity2, (direction2 - direction1) % 5);
+      // entity2.setDirection(direction1);
 
       // Each ship takes damage proportional to the momentum of the other ship
       entity1.damage(speed2 * entity2.getMass() * Parameters.COLLISION_DAMAGE_MODIFIER);
@@ -66,10 +121,9 @@ public class Collision {
     ship.damage(bullet.getDamage());
     // See if ship has been destroyed
     if (ship.getHealth() <= 0) {
-      // ship.destroy();
+      // ship.delete();
     }
     // Destroy bullet
-    // bullet.destroy();
-
+    bullet.delete();
   }
 }
