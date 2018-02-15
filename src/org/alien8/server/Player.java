dbd2aeb554bc903e80ccb8e427a06409d62c46ea
Lingyi Zhang <lxz732@cs.bham.ac.uk>
2018-02-15 01:10:29 +0000

    Player.java add port number

--- src/org/alien8/server/Player.java
+++ src/org/alien8/server/Player.java
@@ -8,11 +8,20 @@ public class Player {
 	
 	private InetAddress ip = null;
 	private Ship ship = null;
+	// add port number
+	private int port = 4400;
 	
 	public Player(InetAddress ip, Ship s) {
 		this.ip = ip;
 		ship = s;
 	}
+	
+	public Player(InetAddress ip, Ship s, int port) {
+		this.ip = ip;
+		this.ship = s;
+		this.port = port;
+	}
+
 	
 	public InetAddress getIP() {
 		return ip;
@@ -20,6 +29,10 @@ public class Player {
 	
 	public Ship getShip() {
 		return ship;
+	}
+	
+	public int getPort() {
+		return this.port;
 	}
 	
 }
