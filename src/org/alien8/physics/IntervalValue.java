8e87b9838b22da7db1c41edd8e61c679b804233e
James Atkin <jamesalexatkin@gmail.com>
2018-03-16 17:40:46 +0000

    Added some JavaDoc to Physics classes

--- src/org/alien8/physics/IntervalValue.java
+++ src/org/alien8/physics/IntervalValue.java
@@ -2,39 +2,54 @@ package org.alien8.physics;
 
 import org.alien8.core.Entity;
 
+/**
+ * This class represents a value for the interval covered by an object. It is to be used as part of
+ * the sort-and-sweep algorithm during collision detection.
+ *
+ */
 public class IntervalValue {
 
   private IntervalValueType type;
   private Entity entity;
   private double value;
 
+  /**
+   * Constructor.
+   * 
+   * @param type the type of the value
+   * @param entity the Entity the value corresponds to
+   * @param value the value itself
+   */
   public IntervalValue(IntervalValueType type, Entity entity, double value) {
-    this.setType(type);
-    this.setEntity(entity);
-    this.setValue(value);
+    this.type = type;
+    this.entity = entity;
+    this.value = value;
   }
 
+  /**
+   * Returns the type of the value, signifying whether it is the beginning or end of an interval.
+   * 
+   * @return the type
+   */
   public IntervalValueType getType() {
     return type;
   }
 
-  public void setType(IntervalValueType type) {
-    this.type = type;
-  }
-
+  /**
+   * Returns the Entity the value corresponds to.
+   * 
+   * @return the Entity the value corresponds to
+   */
   public Entity getEntity() {
     return entity;
   }
 
-  public void setEntity(Entity entity) {
-    this.entity = entity;
-  }
-
+  /**
+   * Returns the value of the IntervalValue.
+   * 
+   * @return the value itself
+   */
   public double getValue() {
     return value;
-  }
-
-  public void setValue(double value) {
-    this.value = value;
   }
 }
--- src/org/alien8/physics/Position.java
+++ src/org/alien8/physics/Position.java
@@ -27,6 +27,60 @@ public class Position implements Serializable{
 	}
 
 	/**
+   * @return the X coordinate
+   */
+  public double getX() {
+  	return x;
+  }
+
+  /**
+   * @param x the X coordinate to set
+   */
+  public void setX(double x) {
+  	this.x = x;
+  }
+
+  /**
+   * @return the Y coordinate
+   */
+  public double getY() {
+  	return y;
+  }
+
+  /**
+   * @param y the Y coordinate to set
+   */
+  public void setY(double y) {
+  	this.y = y;
+  }
+
+  /**
+   * Verifies if two positions in the XY plane are equal.
+   * @param position the position to compare this position to
+   * @return true if the two positions are equal, false otherwise
+   */
+  public boolean equals(Position position) {
+  	if((int) this.getX() == (int) position.getX() && (int) this.getY() == (int) position.getY())
+  		return true;
+  	return false;
+  }
+
+  @Override
+  public String toString() {
+  	return "X: " + this.getX() + " Y: " + this.getY();
+  }
+
+  /**
+   * Simply adds the X's and Y's of the two position to 
+   * 
+   * @param position the second point to add to this one
+   * @return a new position resulting from the two
+   */
+  public Position addPosition(Position position) {
+  	return new Position(this.getX() + position.getX(), this.getY() + position.getY());
+  }
+
+  /**
 	 * Computes the angle between the X axis and the line formed by
 	 * the point at this position and the point at the given position
 	 * 
@@ -37,16 +91,6 @@ public class Position implements Serializable{
 		return FastMath.atan2(						// Intentionally left out intermediary variables for speed
 				position.getX() - this.getX(),  // B(x) - A(x)  
 				position.getY() - this.getY()); // B(y) - A(y) 
-	}
-	
-	/**
-	 * Simply adds the X's and Y's of the two position to 
-	 * 
-	 * @param position the second point to add to this one
-	 * @return a new position resulting from the two
-	 */
-	public Position addPosition(Position position) {
-		return new Position(this.getX() + position.getX(), this.getY() + position.getY());
 	}
 	
 	/**
@@ -71,50 +115,6 @@ public class Position implements Serializable{
 		double x = this.getX();
 		double y = this.getY();
 		if(x < 0 || x > Parameters.MAP_WIDTH || y < 0 || y > Parameters.MAP_HEIGHT)
-			return true;
-		return false;
-	}
-	
-	/**
-	 * @return the X coordinate
-	 */
-	public double getX() {
-		return x;
-	}
-
-	/**
-	 * @param x the X coordinate to set
-	 */
-	public void setX(double x) {
-		this.x = x;
-	}
-
-	/**
-	 * @return the Y coordinate
-	 */
-	public double getY() {
-		return y;
-	}
-
-	/**
-	 * @param y the Y coordinate to set
-	 */
-	public void setY(double y) {
-		this.y = y;
-	}
-	
-	@Override
-	public String toString() {
-		return "X: " + this.getX() + " Y: " + this.getY();
-	}
-	
-	/**
-	 * Verifies if two positions in the XY plane are equal.
-	 * @param position the position to compare this position to
-	 * @return true if the two positions are equal, false otherwise
-	 */
-	public boolean equals(Position position) {
-		if((int) this.getX() == (int) position.getX() && (int) this.getY() == (int) position.getY())
 			return true;
 		return false;
 	}
